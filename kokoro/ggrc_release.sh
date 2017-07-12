8e3667e960bfef71688f5de8aaab7d467580bc10
Saroj Silwal Khatri <skhatri@skhatri.svl.corp.google.com>
2017-07-12 13:30:16 -0700

    Add missing variables

--- kokoro/ggrc_release.sh
+++ kokoro/ggrc_release.sh
@@ -152,16 +152,16 @@ echo $SERVICE_ACCOUNT > "$SERVICE_ACCOUNT_FILE"
 # Fill in private.key with the user's private key
 cat >"$KEY_FILE" <<EOL
 {
-  "type": "service_account",
-  "project_id": "ggrc-dev",
-  "private_key_id": "6602e194b425b0954a5f0434b30ef6d60dc90a54",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDr2CracbgrRTts\nwbrVELWm1MXG11gZYoYtXdglCoyYfHohkNRZ5F1DbYY5xyTCzWL5WvSJemquCQtS\ndkxJzVwcpyM1YAPCIUSFn4DEWctVhSArmUfI7V1N5PFZmZOcdYTBRPv16JsebzHz\nBXe1lIv1fWsEL5Ue1wSvkEgT0QSGPObbSUJWmdkOVp24l+S2JCth+ttZht29PYJE\nqBM5zIWrO32L42j63d1b+lKVTNJQL/XA16Nw6rwFE6ez9JHImDsuAAeTQ0+1Ze0S\n7wEpwkr754OsMcpl8JrxU2Z+lhAxYCRF8ZeM+kfWR1JZlkT4/jLi7Fu4cGvH7NJg\n9kR4UqhBAgMBAAECggEBANmI1NYSKF55CDvjYVIfjH2qKhajEFaxwrNbqP9ZgJ8x\nyXDmZofXlJKaFkF6xxSLXyxC0eVwra+DFhdkmC9GpRykqVwVCMJp7wsVOS9i56Ml\nHKw6QLU7A2HSty7+8eFRoDaoS4LhnxpuU/MlaupobsPrf+cngHMmvuK0wVbN509s\nXeobycD8LOhDl9WLZ6oFJrLH4m39st105OJC+/QyjeMzaMTEsXKRsGv5Fde3xDPn\n5LsVjrk3mmIE/bcinmdwueVR1sBS4bkUAg+GFfuKBLTZZ7WN92qvhuISTEsrjy/6\nJAkNDhk4a5gS9EIyNqG86Mg6X+V9ZoE9J/5Jd/bOIVUCgYEA+VJWxb6wf7203wJ6\n8GRBUDveyC9Skhhc9n3cFN3vIZWVhZm7MNZnEHsWtYvFoDb/Wt6KV9o6CBjy+mYK\ndmVMpIBSxMsbnvYgjmZ3vz+463M0COU1BT0dfi6BOK4ol34F9oCYu8L0srgQi7kj\nPs4wVDgQBHoiEwkFdANWp43LeIMCgYEA8ilpY3BJAB/hgrSXe+jFR7m2iC+rzHIn\n5k+pz+vsfhWIX+nvYGYkykEUhzYDkZtgd0wu/ZgT837ELGzQ0DztBBSRD1ODPwNJ\nKwJMuTX3KWhHFC0+ApOY2ocYKiP1eefBI7MFoMG005gyJDO+dUwhkmYwntlSYfMp\nG24YeDgKWOsCgYEAqL41xHifdJWtCRLgqjrwiaE70zlUJVUf9iSRA/6BjrVzEY1O\nyGsULm9gm1cSVrFietoLwBIPHNPl/9t4UVGWYfAIFPFyrE/hEQABJAu42IKMQUkA\nbZ9Ditdm1jnpdz7wQjofJVV50EwLxsVzOVrMEvQuwxj2XvPIIRDxYU3y3IkCgYEA\nmFISF1k+odRr3fJIQsmEpfwb44fQ0XWQwV6kmsN0a06SDHqydnlpdMsA5ZfFIOaS\nBBgoip0JF6VKMgN0STe5glKJeBF4wb8IXARDTFC0mhgcdYWLtsUuZW6KdZ9OvhJX\nu2PVC2wsmNfn2jut9kwf5d1fgduC5Ve1KKrUu3HMmGcCgYBwmTZLuEaRIF9hMyBF\nqF5yhaQ+jo3H5O1mAwgHqoBedbD/Tse56uzeSOQixVRfZxhGDB8B5iaBTm6tBKVo\nUjO2SEpIQWmqMFKdFJDF1hcUj/paMie8JqK00RbVZVG1XjdpzRz1+hHpVPf4lWXL\nTv5F/57RQtvnHfh5l9AREkczRQ==\n-----END PRIVATE KEY-----\n",
-  "client_email": "test-jenkins@ggrc-dev.iam.gserviceaccount.com",
-  "client_id": "117820491369318290632",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://accounts.google.com/o/oauth2/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test-jenkins%40ggrc-dev.iam.gserviceaccount.com"
+  "type": "$ACCOUNT_TYPE",
+  "project_id": "$PROJECT_ID",
+  "private_key_id": "$PRIVATE_KEY_ID",
+  "private_key": "$PRIVATE_KEY",
+  "client_email": "$CLIENT_EMAIL",
+  "client_id": "$CLIENT_ID",
+  "auth_uri": "$AUTH_URI",
+  "token_uri": "$TOKEN_URI",
+  "auth_provider_x509_cert_url": "$AUTH_PROVIDER_X509_CERT_URL",
+  "client_x509_cert_url": "$CLIENT_X509_CERT_URL"
 }
 EOL
 
